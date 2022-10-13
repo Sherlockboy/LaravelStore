5a048b4793a785e495b42b59bc3ebd3407d85de8
Usievalad Haponienka <usievalad.haponienka@gmail.com>
2022-10-13 18:48:23 +0500

    - Laravel store
    Address controller refactoring

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -4,41 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\Address;
 use App\Rules\PhoneNumber;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
 
 class AddressController extends Controller
 {
+    /**
+     * @return Application|Factory|View
+     */
     public function index()
     {
         $user = auth()->user();
         return view('user.account.address.index', compact('user'));
     }
 
+    /**
+     * @return Application|RedirectResponse|Redirector
+     */
     public function store()
     {
         $data = $this->prepareAddressData();
-
         $this->resolveOnlyOneDefaultAddress($data);
 
         $data['user_id'] = auth()->user()->id;
-
-        if ($data['is_default']) {
-            foreach (auth()->user()->addresses as $address) {
-                if ($address->is_default) {
-                    $address->update(['is_default' => 0]);
-                }
-            }
-        }
 
         Address::create($data);
 
         return redirect(url()->previous());
     }
 
+    /**
+     * @param Address $address
+     * @return Application|RedirectResponse|Redirector
+     */
     public function update(Address $address)
     {
         $data = $this->prepareAddressData();
 
-        if (!($address->is_default && $data['is_default'])) {
+        if ((!$address->is_default && $data['is_default'])) {
             $this->resolveOnlyOneDefaultAddress($data);
         }
 
@@ -47,6 +54,10 @@ class AddressController extends Controller
         return redirect(url()->previous());
     }
 
+    /**
+     * @param Address $address
+     * @return JsonResponse
+     */
     public function delete(Address $address)
     {
         $addressTitle = $address->title;
@@ -54,17 +65,27 @@ class AddressController extends Controller
         return response()->json(['title' => $addressTitle]);
     }
 
+    /**
+     * As user can have only one default address, previous default address should be marked as non-default.
+     * @param $data
+     * @return void
+     */
     private function resolveOnlyOneDefaultAddress($data): void
     {
         if ($data['is_default']) {
-            foreach (auth()->user()->addresses as $address) {
-                if ($address->is_default) {
-                    $address->update(['is_default' => 0]);
-                }
-            }
+            $address = Address::where('user_id', '=', auth()->user()->id)
+                ->where('is_default', '=', 1)
+                ->get()
+                ->first();
+
+            $address->update(['is_default' => 0]);
         }
     }
 
+    /**
+     * Prepare address data before creating/updating address
+     * @return array
+     */
     private function prepareAddressData(): array
     {
         $data = request()->validate([
