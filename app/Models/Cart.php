92943452a194bf8c7ebeb5e8fc923ea7b9c5477b
Usievalad Haponienka <usievalad.haponienka@gmail.com>
2022-10-17 13:10:21 +0500

    - Laravel store
    Address and Cart model refactoring. Cart merge fixed.

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -11,22 +11,36 @@ class Cart extends Model
 {
     use HasFactory;
 
+    /**
+     * @inheritdoc
+     */
     protected $fillable = [
         'user_id',
         'is_guest',
         'session_id'
     ];
 
+    /**
+     * @return BelongsTo
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * @return HasMany
+     */
     public function items(): HasMany
     {
         return $this->hasMany(CartItem::class);
     }
 
+    /**
+     * Get total cart price
+     * 
+     * @return int
+     */
     public function getFinalPrice(): int
     {
         $finalPrice = 0;
@@ -37,27 +51,75 @@ class Cart extends Model
         return $finalPrice;
     }
 
-    public function clearCart()
+    /**
+     * Remove all items from cart
+     *
+     * @return void
+     */
+    public function clearCart(): void
     {
         foreach ($this->items as $cartItem) {
             $cartItem->delete();
         }
     }
 
+    /**
+     * Merge guest and user cart after login.
+     * Cart items for guest cart are added user cart.
+     * If product is already in user cart, product qty in user will be increased by guest cart product qty
+     *
+     * @param Cart $guestCart
+     * @return void
+     */
     public static function merge(Cart $guestCart): void
     {
+        /** @var Cart $userCart */
         $userCart = auth()->user()->cart;
-        /** @var CartItem $item */
-        foreach ($guestCart->items as $item) {
-            $item->update([
+
+        /** @var CartItem $guestCartItem */
+        foreach ($guestCart->items as $guestCartItem) {
+            $cartItemUpdateData = [
                 'cart_id' => $userCart->id
-            ]);
+            ];
+
+            $userCartItem = $userCart->getCartItemByProductId($guestCartItem->product->id);
+
+            if ($userCartItem) {
+                // If user has same product in the cart, result qty must be equal to guest gat qty + user cart qty
+                $cartItemUpdateData['qty'] = $guestCartItem->qty + $userCartItem->qty;
+                $userCartItem->delete();
+            }
+
+            $guestCartItem->update($cartItemUpdateData);
         }
 
         $guestCart->delete();
     }
 
-    public static function getCart($createNew = true): ?Cart
+    /**
+     * Get cart related to product with id = $productId
+     * Returns null if there is no such cart item
+     *
+     * @param int $productId
+     * @return CartItem|null
+     */
+    public function getCartItemByProductId(int $productId): ?CartItem
+    {
+        return $this->items->where('product_id', '=', $productId)->first();
+    }
+
+    /**
+     * TODO: rewrite guest cart using cookie instead of session
+     * Get cart instance
+     * For authorized user returns user cart
+     * For unauthorized user returns guest cart (by session id).
+     * If guest cart doesn't exist, creates new guest cart and returns it.
+     * If $createNew param == false, returns null instead of creating new guest cart
+     *
+     * @param bool $createNew
+     * @return Cart|null
+     */
+    public static function getCart(bool $createNew = true): ?Cart
     {
         if (auth()->user()) {
             //Get user cart for authorized user
